e80573a014ff1d3c79a6786cc211559986f03d81
Alexander Hoffert <alexander.hoffert@sap.com>
2021-12-02 10:33:48 +0100

    day 2

--- src/advent_of_code/day_02.clj
+++ src/advent_of_code/day_02.clj
@@ -1,11 +1,41 @@
 (ns advent-of-code.day-02)
+(require '[clojure.string :as string])
+
+(defn parse-int [str] (#(Integer. str)))
+
+(defn parse-line [line]
+  ((fn [[direction amount]] [direction (parse-int amount)])
+   (string/split line #" ")))
+
+(defn read-input [input]
+  (map parse-line (string/split input #"\n")))
+
+(defn get-position [input]
+  (reduce
+   (fn [[x y] [direction amount]]
+     (cond
+       (= direction "up") [x (- y amount)]
+       (= direction "down") [x (+ y amount)]
+       :else [(+ x amount) y]))
+   [0 0]
+   (read-input input)))
+
+(defn get-position-with-aim [input]
+  (reduce
+   (fn [[x y aim] [direction amount]]
+     (cond
+       (= direction "up") [x y (- aim amount)]
+       (= direction "down") [x y (+ aim amount)]
+       :else [(+ x amount) (+ y (* aim amount)) aim]))
+   [0 0 0]
+   (read-input input)))
 
 (defn part-1
   "Day 02 Part 1"
   [input]
-  input)
+  (#(* (first %) (second %)) (get-position input)))
 
 (defn part-2
   "Day 02 Part 2"
   [input]
-  input)
+  (#(* (first %) (second %)) (get-position-with-aim input)))
