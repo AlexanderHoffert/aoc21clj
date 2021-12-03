4c9476781a2cdbe83f1be20675bc0ae4bb605857
Alexander Hoffert <alexander.hoffert@sap.com>
2021-12-03 07:10:39 +0100

    day 3 p1

--- src/advent_of_code/day_03.clj
+++ src/advent_of_code/day_03.clj
@@ -1,11 +1,41 @@
 (ns advent-of-code.day-03)
+(require '[clojure.string :as string])
+
+(defn read-input [input]
+  (string/split input #"\n"))
+
+(defn count-1-bits [input]
+  (reduce (fn [sums line]
+            (map-indexed (fn [index sum]
+                           (+ sum
+                              (Character/digit (nth line index) 2)))
+                         sums))
+          (repeat (count (first input)) 0)
+          input))
+
+(defn get-number [half gamma counts]
+  (Integer/parseInt
+   (String/join "" (map #(if (or
+                              (and gamma (> % half))
+                              (and (not gamma) (< % half)))
+                           "1" "0")
+                        counts))
+   2))
+
+(defn calc [half counts]
+  (*
+   (get-number half true counts)
+   (get-number half false counts)))
 
 (defn part-1
   "Day 03 Part 1"
   [input]
-  input)
+  (let [lines (read-input input)]
+    (->> lines
+         count-1-bits
+         (calc (/ (count lines) 2)))))
 
 (defn part-2
   "Day 03 Part 2"
   [input]
-  input)
+  0)
--- test/advent_of_code/day_03_test.clj
+++ test/advent_of_code/day_03_test.clj
@@ -4,7 +4,7 @@
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
+  (let [expected 198]
     (is (= expected (part-1 (slurp (resource "day-03-example.txt")))))))
 
 (deftest part2
