a9be5c17c8ad08ca84967f50b53e2219e5f906a9
Alexander Hoffert <alexander.hoffert@sap.com>
2021-12-07 11:02:14 +0100

    day 4 part 1

--- src/advent_of_code/day_04.clj
+++ src/advent_of_code/day_04.clj
@@ -22,10 +22,33 @@
                  (string/split #"\n\n")
                  (->> (mapv parse-board)))}))
 
+(defn transpose [board]
+  (apply map vector board))
+
+(defn get-rows-score [pick-list board]
+  (let [numbers (set pick-list)]
+    (when (some #(every? numbers %) board)
+      (->> board
+           (apply concat)
+           (remove numbers)
+           (apply +)
+           (* (last pick-list))))))
+
+(defn get-board-score [pick-list board]
+  (or (get-rows-score pick-list board)
+      (get-rows-score pick-list (transpose board))))
+
+(defn get-score [pick-list boards]
+  (some #(get-board-score pick-list %) boards))
+
 (defn part-1
   "Day 04 Part 1"
   [input]
-  (parse-input input))
+  (let [{:keys [numbers boards]} (parse-input input)]
+    (loop [n 1]
+      (let [pick-list (take n numbers)]
+        (or (get-score pick-list boards)
+            (recur (inc n)))))))
 
 (defn part-2
   "Day 04 Part 2"
