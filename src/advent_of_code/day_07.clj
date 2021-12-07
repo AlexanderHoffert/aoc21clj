cb6f3562df09b7ac4f41fb3dcef68864b4f9074b
Alexander Hoffert <alexander.hoffert@sap.com>
2021-12-07 09:19:03 +0100

    day 7

--- src/advent_of_code/day_07.clj
+++ src/advent_of_code/day_07.clj
@@ -1,11 +1,43 @@
-(ns advent-of-code.day-07)
+(ns advent-of-code.day-07
+  (:require [clojure.string :as string]))
+
+(defn parse-input [input]
+  (map #(Integer/parseInt %) (string/split input #",")))
+
+(defn calc-consumption [variant value]
+  (if (= variant :a)
+    value
+    (/ (* value (inc value)) 2)))
+
+(defn get-comsumption [variant positions x]
+  (for [position positions]
+    (->> position
+         (- x)
+         Math/abs
+         (calc-consumption variant))))
+
+(defn get-comsumptions [variant max positions]
+  (for [x (range max)]
+    (->> x
+         (get-comsumption variant positions)
+         (apply +))))
+
+(defn calculate-fuel [variant positions]
+  (let [max-val (apply max positions)]
+    (->> positions
+         (get-comsumptions variant max-val)
+         (apply min))))
 
 (defn part-1
   "Day 07 Part 1"
   [input]
-  input)
+  (->> input
+       parse-input
+       (calculate-fuel :a)))
 
 (defn part-2
   "Day 07 Part 2"
   [input]
-  input)
+  (->> input
+       parse-input
+       (calculate-fuel :b)))
--- test/advent_of_code/day_07_test.clj
+++ test/advent_of_code/day_07_test.clj
@@ -4,9 +4,9 @@
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
+  (let [expected 37]
     (is (= expected (part-1 (slurp (resource "day-07-example.txt")))))))
 
 (deftest part2
-  (let [expected nil]
+  (let [expected 168]
     (is (= expected (part-2 (slurp (resource "day-07-example.txt")))))))
