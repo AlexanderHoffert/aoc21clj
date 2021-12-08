279aa40717393d4bb077cd44f5f599b80bb799fa
Alexander Hoffert <alexander.hoffert@sap.com>
2021-12-08 06:39:35 +0100

    day 8 p1

--- src/advent_of_code/day_08.clj
+++ src/advent_of_code/day_08.clj
@@ -1,11 +1,40 @@
-(ns advent-of-code.day-08)
+(ns advent-of-code.day-08
+  (:require [clojure.string :as string]))
+
+(defn parse-segment [segment]
+  (-> segment
+      (string/trim)
+      (string/split #"\s+")))
+
+(defn parse-line [line]
+  (->> (string/split line #"\|")
+       (mapv parse-segment)
+       (zipmap [:signals :output])))
+
+(defn parse-input [input]
+  (->> (string/split input #"\n")
+       (mapv parse-line)))
+
+(def easy-values #{2 3 4 7})
+
+(defn count-output-digits [output]
+  (count (filter #(contains? easy-values (count %)) output)))
+
+(defn count-digits [lines]
+  (reduce (fn [acc line] (+ acc (count-output-digits (line :output))))
+          0 lines))
 
 (defn part-1
   "Day 08 Part 1"
   [input]
-  input)
+  (->> (parse-input input)
+       (count-digits)))
 
 (defn part-2
   "Day 08 Part 2"
   [input]
   input)
+
+(comment
+  (parse-segment "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb")
+  (parse-line "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"))
--- test/advent_of_code/day_08_test.clj
+++ test/advent_of_code/day_08_test.clj
@@ -4,7 +4,7 @@
             [clojure.java.io :refer [resource]]))
 
 (deftest part1
-  (let [expected nil]
+  (let [expected 26]
     (is (= expected (part-1 (slurp (resource "day-08-example.txt")))))))
 
 (deftest part2
