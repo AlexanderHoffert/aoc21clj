add8a0aa64a8a4e8b3124f40b730bce6e8cec7c6
Alexander Hoffert <alexander.hoffert@sap.com>
2021-12-10 10:33:46 +0100

    day 08 p2

--- src/advent_of_code/day_08.clj
+++ src/advent_of_code/day_08.clj
@@ -1,10 +1,12 @@
 (ns advent-of-code.day-08
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [clojure.set :refer [difference subset?]]))
 
 (defn parse-segment [segment]
   (-> segment
       (string/trim)
-      (string/split #"\s+")))
+      (string/split #"\s+")
+      (->> (mapv set))))
 
 (defn parse-line [line]
   (->> (string/split line #"\|")
@@ -30,11 +32,47 @@
   (->> (parse-input input)
        (count-digits)))
 
+(defn get-counts [signals]
+  (zipmap (range 2 8) (for [c (range 2 8)] (set (filter #(= (count %) c) signals)))))
+
+(defn get-numbers [counts]
+  (let [one (first (counts 2))
+        four (first (counts 4))
+        seven (first (counts 3))
+        eight (first (counts 7))
+        six (some #(when (not (subset? one %)) %) (counts 6))
+        three (some #(when (subset? one %) %) (counts 5))
+        nine (some #(when (subset? three %) %) (counts 6))
+        zero (first (difference (counts 6) #{six nine}))
+        segment-b (first (difference nine three))
+        five (some #(when (contains? % segment-b) %) (counts 5))
+        two (first (difference (counts 5) #{three five}))]
+    {one 1 two 2 three 3 four 4 five 5 six 6 seven 7 eight 8 nine 9 zero 0}))
+
+(defn get-output-value
+  [line]
+  (->> (line :signals)
+       get-counts
+       get-numbers
+       (#(map % (line :output)))
+       string/join
+       Integer/parseInt))
+
+(defn get-output-values [lines]
+  (apply + (map get-output-value lines)))
+
 (defn part-2
   "Day 08 Part 2"
   [input]
-  input)
+  (->> input
+       parse-input
+       get-output-values))
 
 (comment
   (parse-segment "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb")
-  (parse-line "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"))
+  (parse-line "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe")
+  (let [line (parse-line "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg")
+        signals (:signals line)
+        counts (get-counts signals)
+        numbers (get-numbers counts)]
+    numbers))
--- test/advent_of_code/day_08_test.clj
+++ test/advent_of_code/day_08_test.clj
@@ -8,5 +8,5 @@
     (is (= expected (part-1 (slurp (resource "day-08-example.txt")))))))
 
 (deftest part2
-  (let [expected nil]
+  (let [expected 61229]
     (is (= expected (part-2 (slurp (resource "day-08-example.txt")))))))
