f3943daf24524ff1cac70a05664b1e8273c516f7
Alexander Hoffert <alexander.hoffert@sap.com>
2021-12-14 17:57:03 +0100

    day 14 p2

--- src/advent_of_code/day_14.clj
+++ src/advent_of_code/day_14.clj
@@ -9,63 +9,61 @@
 (defn parse-input [input]
   (let [[template rules] (string/split input #"\n\n")]
     (hash-map
-     :template (string/split template #"")
+     :pairs (-> template
+                (->> (partition 2 1 " ") ; add padding to remain the last char
+                     (map string/join))
+                frequencies)
      :rules (->> rules
                  string/split-lines
                  (map parse-rule)
                  (#(zipmap (map first %) (map second %)))))))
 
-(defn apply-rules [rules segments]
-  (->> segments
-       (map (fn [segment]
-              (let [new-char (get rules (string/join segment))]
+(defn apply-rules [rules pairs]
+  (->> pairs
+       (map (fn [pair]
+              (let [[chars count] pair
+                    new-char (get rules chars)]
                 (if (nil? new-char)
-                  [(first segment)]
-                  [(first segment) new-char]))))
-       (apply concat)))
+                  (reduce hash-map pair)
+                  (let [w1 (str (first chars) new-char)
+                        w2 (str new-char (second chars))]
+                    (if (= w1 w2)
+                      {w1 (* count 2)}
+                      {w1 count w2 count}))))))
+       (apply merge-with +)))
 
-(defn apply-rules-10-times [{initial-template :template rules :rules}]
+(defn apply-rules-times [times {initial-pairs :pairs rules :rules}]
   (loop [count 0
-         template initial-template]
-    (if (= count 20)
-      template
+         pairs initial-pairs]
+    (if (= count times)
+      pairs
       (recur
        (inc count)
-       (->> template
-            (partition 2 1 " ")
-            (apply-rules rules))))))
+       (apply-rules rules pairs)))))
+
+(defn sum-up-single-chars [char-pair-map]
+  (reduce
+   (fn [sums pair]
+     (let [char (first (first pair))
+           old-count (or (get sums char) 0)]
+       (assoc sums char (+ old-count (second pair)))))
+   {}
+   char-pair-map))
+
+(defn calc-result [input times]
+  (->> input
+       parse-input
+       (apply-rules-times times)
+       sum-up-single-chars
+       (map second)
+       (#(- (reduce max %) (reduce min %)))))
 
 (defn part-1
   "Day 14 Part 1"
   [input]
-  (time (->> input
-             parse-input
-             apply-rules-10-times
-             frequencies
-             (map second)
-             (#(- (reduce max %) (reduce min %))))))
+  (calc-result input 10))
 
 (defn part-2
   "Day 14 Part 2"
   [input]
-  nil)
-
-(comment
-  (let [rules (parse-input "NNCB
-
-CH -> B
-HH -> N
-CB -> H
-NH -> C
-HB -> C
-HC -> B
-HN -> C
-NN -> C
-BH -> H
-NC -> B
-NB -> B
-BN -> B
-BB -> N
-BC -> B
-CC -> N
-CN -> C")] rules))
+  (time (calc-result input 40)))
--- test/advent_of_code/day_14_test.clj
+++ test/advent_of_code/day_14_test.clj
@@ -8,5 +8,5 @@
     (is (= expected (part-1 (slurp (resource "day-14-example.txt")))))))
 
 (deftest part2
-  (let [expected nil]
+  (let [expected 2188189693529]
     (is (= expected (part-2 (slurp (resource "day-14-example.txt")))))))
